414fdcb6f13f6e4ba24606068a68a546a069b5f9
writeens <44687555+writeens@users.noreply.github.com>
2019-05-31 17:47:09 +0100

    Updates

---
+++ precache-manifest.b19c65d1a370cf8aca3b55a829b9f497.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1930ef858b09085e64522aa3aa48938",
+    "url": "/appy/index.html"
+  },
+  {
+    "revision": "20a7055c7f7dc9744765",
+    "url": "/appy/static/css/main.faf56910.chunk.css"
+  },
+  {
+    "revision": "91617c1f806e8d62236a",
+    "url": "/appy/static/js/2.22d82475.chunk.js"
+  },
+  {
+    "revision": "20a7055c7f7dc9744765",
+    "url": "/appy/static/js/main.a61ab1ff.chunk.js"
+  },
+  {
+    "revision": "4d51df340d972fc1d498",
+    "url": "/appy/static/js/runtime~main.94ecb003.js"
+  }
+]);
